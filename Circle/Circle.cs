27a93290c677d91a8502e5d4e40764d3183f1faa
Anton Larsson <anton.cj.sk8@gmail.com>
2023-10-20 14:30:09 +0200

    Add project files.

---
+++ Circle/Circle.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Circle
+{
+    internal class Circle           //Class
+    {
+        public float _pi = 3.141f;
+        public int _radius;
+
+
+     
+        public Circle(float Pi, int Rad)    //Constructor
+        {
+            _pi = Pi;
+            _radius = Rad;
+            
+        }
+              
+        public void getArea()       //method
+        {                      
+            Console.WriteLine("Arean är: {0}",_radius * _radius * _pi);
+        }
+    }        
+}
---
+++ Circle/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace Circle
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Circle circle1 = new Circle(3.141f, 5); //this will calculate the circle of 5
+            circle1.getArea();
+
+            Circle circle2 = new Circle(3.141f, 6); //this will calculate the circle of 6
+            circle2.getArea();
+        }
+    }
+}
